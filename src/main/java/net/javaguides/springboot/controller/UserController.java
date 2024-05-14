8d5d71dc6ee29d8df5acdaf9ff1559d98d17659e
Dewald15 <vdbergdewald@gmail.com>
2024-05-14 13:19:30 +0200

    Refactored Get All Users REST API to use DTO

--- src/main/java/net/javaguides/springboot/controller/UserController.java
+++ src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -33,8 +33,8 @@ public class UserController {
 
     // http://localhost:8080/api/users
     @GetMapping
-    public ResponseEntity<List<User>> getAllUsers(){
-        List<User> users = userService.getAllUsers();
+    public ResponseEntity<List<UserDto>> getAllUsers(){
+        List<UserDto> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
